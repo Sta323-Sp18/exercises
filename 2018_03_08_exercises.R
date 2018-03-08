8d0a2e1fd2cd360fbbc965993f9e35714f5f9158
Colin Rundel <rundel@gmail.com>
2018-03-08 12:45:38 -0500

    class

---
+++ 2018_03_08_exercises.R
@@ -0,0 +1,27 @@
+library(httr)
+library(glue)
+library(jsonlite)
+library(magrittr)
+library(purrr)
+
+
+## Google Maps Example - https://developers.google.com/maps/documentation/geocoding/intro
+
+google_key = readLines("~/tmp_google_key")
+
+
+
+## Github API - https://developer.github.com/v3/
+
+github_token = readLines("~/tmp_token")
+
+
+### User Info
+
+
+### Org Info
+
+
+
+### Repo
+
