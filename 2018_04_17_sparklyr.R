7a62d919cbc5c7dde24b3b1a7b7f2194baa1fa72
Colin Rundel <rundel@gmail.com>
2018-04-19 14:52:15 -0400

    in class

--- 2018_04_17_sparklyr.R
+++ 2018_04_17_sparklyr.R
@@ -2,6 +2,7 @@ library(sparklyr)
 library(dplyr)
 library(dbplyr)
 library(stringr)
+library(ggplot2)
 
 #spark_install()
 #spark_available_versions()
@@ -12,8 +13,8 @@ sc = spark_connect(master = "local[4]", spark_home = "/data/spark/spark-2.2.1-bi
 #system.time({green = readr::read_csv("/data/nyc-taxi-data/nyc_taxi_2017/green_tripdata_2017-01.csv")})
 #system.time({yellow = readr::read_csv("/data/nyc-taxi-data/nyc_taxi_2017/yellow_tripdata_2017-01.csv")})
 
-system.time({green = spark_read_csv(sc, "green", path = "/data/nyc-taxi-data/nyc_taxi_2017/green_tripdata_2017-01.csv")})
-system.time({yellow = spark_read_csv(sc, "yellow", path = "/data/nyc-taxi-data/nyc_taxi_2017/yellow_tripdata_2017-01.csv")})
+green = spark_read_csv(sc, "green", path = "/data/nyc-taxi-data/nyc_taxi_2017/green_tripdata_2017-01.csv")
+yellow = spark_read_csv(sc, "yellow", path = "/data/nyc-taxi-data/nyc_taxi_2017/yellow_tripdata_2017-01.csv")
 
 fix_colnames = function(df) {
   colnames(df) %>%
@@ -48,10 +49,10 @@ wday_hour_summary = function(df, label) {
     ) %>%
     group_by(hour, wday) %>%
     summarize(
-      avg_dist = mean(trip_distance),
-      avg_time = mean(trip_time),
-      avg_fare = mean(fare_amount),
-      avg_tip_perc = mean(tip_amount / fare_amount)
+      avg_dist = mean(trip_distance, na.rm=TRUE),
+      avg_time = mean(trip_time, na.rm=TRUE),
+      avg_fare = mean(fare_amount, na.rm=TRUE),
+      avg_tip_perc = mean(tip_amount / fare_amount, na.rm=TRUE)
     ) %>%
     mutate(type = label) %>%
     collect()
@@ -60,18 +61,102 @@ wday_hour_summary = function(df, label) {
 green_wday = wday_hour_summary(green, "green")
 yellow_wday = wday_hour_summary(yellow, "yellow")
 
-
 bind_rows(
   green_wday,
   yellow_wday
 ) %>%
   ungroup() %>%
-  tidyr::gather(var, value, starts_with("avg")) %>%
+  tidyr::gather(quantity, value, starts_with("avg")) %>%
   mutate(wday = factor(wday, levels = c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))) %>%
-  ggplot(aes(color = type, x=hour, y=value)) +
+  ggplot(aes(x=hour, y=value, color=type)) +
     geom_line(size=1.5) +
-    facet_grid(var~wday, scale="free_y") +
-    scale_color_manual(values=c('green','yellow')) +
-    labs(y="")
+    facet_grid(quantity ~ wday, scales = "free_y") + 
+    scale_color_manual(values=c("green","yellow"))
 
+### Zones
+
+zones = sf::st_read("/data/nyc-taxi-data/taxi_zones/")
+
+plot(sf::st_geometry(zones))
+
+
+
+
+
+## Reddit data
+
+reddit = spark_read_json(sc, "reddit", "/data/reddit/RC_2017-10-smaller")
+
+### Popular subreddits
+
+reddit %>% 
+  select(author, subreddit, body) %>%
+  group_by(subreddit) %>%
+  summarize(n = n()) %>%
+  arrange(desc(n)) %>%
+  head(50) %>%
+  collect()
+
+### Most posts
+
+reddit %>% 
+  select(author, subreddit, body) %>%
+  group_by(author) %>%
+  summarize(n = n()) %>%
+  arrange(desc(n)) %>%
+  head(50) %>%
+  collect()
+
+### When do people post
+
+reddit %>% 
+  select(author, subreddit, body, created_utc) %>% 
+  mutate(hour = hour(from_unixtime(created_utc))) %>%
+  count(hour) %>%
+  arrange(hour) %>%
+  collect() %>%
+  plot(type='l')
+
+
+### 
+
+library(tidytext)
+
+reddit %>% 
+  select(author, subreddit, body) %>%
+  head(10000) %>%
+  collect() %>%
+  mutate(text = str_split(body," ")) %>%
+  tidyr::unnest() %>%
+  mutate(
+    text = tolower(text),
+    text = str_replace_all(text, "[^a-z]", "")
+  ) %>%
+  count(text) %>% 
+  arrange(desc(n)) %>%
+  mutate(word = text) %>%
+  anti_join(tidytext::get_stopwords())
+
+reddit %>% 
+  select(author, subreddit, body) %>%
+  mutate(body = regexp_replace(body, "[^A-Za-z ]","")) %>%
+  ft_tokenizer("body", "tokens") %>%
+  ft_stop_words_remover("tokens", "word") %>%
+  mutate(word = explode(word)) %>%
+  count(word) %>%
+  arrange(desc(n))
+
+
+## Hamlet example
+
+hamlet = spark_read_text(sc, "hamlet", "/data/Shakespeare/hamlet.txt")
+
+hamlet %>% 
+  mutate(line = regexp_replace(line, "[^A-Za-z ]","")) %>%
+  ft_tokenizer("line", "tokens") %>%
+  ft_stop_words_remover("tokens", "word") %>%
+  mutate(word = explode(word)) %>%
+  count(word) %>%
+  arrange(desc(n))
   
+ 
